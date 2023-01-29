5fb4aa34b56b2a2accc9eb612b515eea3a038ce6
BrownCarter <lilbrown3000@gmail.com>
2023-01-29 23:48:47 +0100

    fix node.hpp

--- include/node.hpp
+++ include/node.hpp
@@ -2,13 +2,6 @@
 #include <memory>
 struct Node {
   int data;
-<<<<<<< HEAD
-  Node *next;
   Node(int data);
-=======
   std::unique_ptr<Node> next;
-
-public:
-  Node(int data) : data{data}, next{nullptr} {};
->>>>>>> 5235024 (added more functions)
 };
