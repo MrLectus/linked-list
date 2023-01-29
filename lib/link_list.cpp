32694409dd617367d07a627bf9d422b9236a364e
BrownCarter <lilbrown3000@gmail.com>
2023-01-29 22:29:39 +0100

    using smart pointers

--- lib/link_list.cpp
+++ lib/link_list.cpp
@@ -1,73 +1,134 @@
 #include "link_list.hpp"
 #include "node.hpp"
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <utility>
 
-Linklist::Linklist(int data) {
-  head = new Node(data);
-  this->size = 1;
+Linklist::Linklist() { head = nullptr; };
+
+void Linklist::push_front(int data) {
+  auto temp = std::make_unique<Node>(data);
+  if (head)
+    temp->next = std::move(head);
+  head = std::move(temp);
+  size++;
 }
 
 void Linklist::push_back(int data) {
-  Node *temp = new Node(data);
-  Node *curr = head;
-  while (curr->next != nullptr) {
-    curr = curr->next;
-  }
-  curr->next = temp;
-  this->size++;
-}
-
-void Linklist::push_front(int data) {
-  Node *temp = new Node(data);
-  temp->next = head;
-  head = temp;
-  this->size++;
-}
-
-size_t Linklist::length() { return this->size; }
-
-void Linklist::insert(size_t index, int data) {
-  Node *temp = new Node(data);
-  Node *prev = head;
-  Node *curr = head->next;
-  if (index == 0) {
-    this->push_front(data);
-  }
-  if (index > size) {
-    this->push_back(data);
+  auto temp = std::make_unique<Node>(data);
+  auto curr = head.get();
+  if (head == nullptr) {
+    head = std::move(temp);
+    size++;
   } else {
-    for (size_t i = 0; i < index - 1; i++) {
-      curr = curr->next;
-      prev = prev->next;
+    while (curr->next != nullptr) {
+      curr = curr->next.get();
     }
-    temp->next = curr;
-    prev->next = temp;
+    curr->next = std::move(temp);
     size++;
   }
 }
 
+void Linklist::insert(size_t index, int data) {
+  auto temp = std::make_unique<Node>(data);
+  if (index <= 0) {
+    push_front(data);
+    size++;
+  } else if (index > size) {
+    push_back(data);
+    size++;
+  }
+  auto prev = head.get();
+  for (size_t i = 0; i < index - 1; ++i) {
+    prev = prev->next.get();
+  }
+  temp->next = std::move(prev->next);
+  prev->next = std::move(temp);
+  size++;
+}
+
 void Linklist::pop_front() {
-  head = head->next;
-  this->size--;
+  if (head == nullptr)
+    return;
+  head = std::move(head->next);
+  size--;
 }
 
 void Linklist::pop_back() {
-  Node *curr = head;
-  for (size_t i = 1; i < this->size - 1; i++) {
-    curr = curr->next;
+  if (head == nullptr)
+    return;
+  auto curr = head.get();
+  for (size_t i = 1; i < size - 1; ++i) {
+    curr = curr->next.get();
   }
   curr->next = nullptr;
-  this->size--;
+  size--;
 }
 
-void Linklist::print() {
-  while (head != nullptr) {
-    std::cout << head->data << " ";
-    head = head->next;
+void Linklist::remove(size_t index) {
+  if (head == nullptr)
+    return;
+  if (index == 0)
+    pop_front();
+  if (index > size)
+    pop_back();
+  else {
+    auto curr = head.get();
+    for (size_t i = 1; i < index; ++i) {
+      curr = curr->next.get();
+    }
+    curr->next = std::move(curr->next->next);
+    size--;
   }
-  std::cout << "\n";
 }
+
+size_t Linklist::get(int index) {
+  if (head == nullptr)
+    return 0;
+  if ((size_t)index >= size) {
+    exit(-1);
+  }
+  auto curr = head.get();
+  for (int i = 0; i < index; ++i) {
+    curr = curr->next.get();
+  }
+  return curr->data;
+}
+
+size_t Linklist::search(int data) {
+  if (head == nullptr) {
+    return -1;
+  }
+  if (head->data == data)
+    return 0;
+  auto curr = head.get();
+  for (size_t i = 0; i < size; ++i) {
+    if (curr->data == data) {
+      return i;
+    }
+    curr = curr->next.get();
+  }
+  return -1;
+}
+
+size_t Linklist::length() { return size; }
+void Linklist::print() {
+  auto temp = head.get();
+  while (temp) {
+    std::cout << temp->data << " ";
+    temp = temp->next.get();
+  }
+
+  std::cout << "\nsize is (" << size << ")";
+}
+
+bool Linklist::empty() { return size == 0; }
 
 Linklist::~Linklist() {
-  std::cout << "memory freed"
-            << "\n";
+  while (head) {
+    head = std::move(head->next);
+  }
 }
