89b85eb74bab127151359f2397da7f71d46b70e0
BrownCarter <lilbrown3000@gmail.com>
2023-01-29 23:38:29 +0100

    .

--- include/link_list.hpp
+++ include/link_list.hpp
@@ -1,21 +1,24 @@
 #pragma once
 #include "node.hpp"
 #include <iostream>
+#include <memory>
 
 class Linklist {
-  Node *head;
-  size_t size;
+  std::unique_ptr<Node> head;
+  size_t size{};
 
 public:
-  Linklist(int data);
+  Linklist();
   void push_back(int data);
   void pop_back();
   void push_front(int data);
   void pop_front();
   void remove(size_t index);
   size_t search(int data);
+  size_t get(int index);
   void insert(size_t index, int data);
   size_t length();
+  bool empty();
   void print();
   ~Linklist();
 };
