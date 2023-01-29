4640d5840249219872a8e05175d80ca2c0f0f005
BrownCarter <lilbrown3000@gmail.com>
2023-01-29 22:29:48 +0100

    added test case

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
 #include "link_list.hpp"
 #include <memory>
 int main() {
-  std::unique_ptr<Linklist> list(new Linklist(400));
-  list->push_back(20);
-  list->push_back(45);
-  list->push_back(56);
-  list->push_front(666);
-  list->insert(100, 179);
-  list->insert(1, 205);
-  list->pop_front();
-  list->pop_front();
-  list->pop_back();
+  std::unique_ptr<Linklist> list(new Linklist());
+  list->push_back(21);
+  list->push_back(34);
+  list->push_back(70);
+  list->push_back(100);
+  list->push_back(200);
+  list->push_back(300);
+  std::cout << "value: " << list->search(100) << "\n";
   list->print();
-  std::cout << "length is (" << list->length() << ")";
-  std::cout << "\n";
 }
